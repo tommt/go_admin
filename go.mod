964eb051e59fdacb3c81adfd37952c45fea3ce73
tommt <atombombmount@gmail.com>
2021-06-16 20:05:00 +0545

    add postgres on docker and connection setup

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module go_admin
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.12.0
+require (
+	github.com/gofiber/fiber/v2 v2.12.0
+	github.com/jackc/pgproto3/v2 v2.1.0 // indirect
+	github.com/lib/pq v1.6.0 // indirect
+	gorm.io/driver/postgres v1.1.0 // indirect
+	gorm.io/gorm v1.21.11 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
+	dsn := "host=db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Kathmandu"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	sqlDB, err := db.DB()
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+
+	if err != nil {
+		panic("Could not connect with the database")
+	}
+	sqlDB.SetMaxIdleConns(10)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
