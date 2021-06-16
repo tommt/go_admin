b884f6dac5efb1f20f498a2646b61444dea22c76
tommt <atombombmount@gmail.com>
2021-06-16 20:11:42 +0545

    change db

--- main.go
+++ main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
+	"go_admin/src/database"
+
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "host=db user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Kathmandu"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	sqlDB, err := db.DB()
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-
-	if err != nil {
-		panic("Could not connect with the database")
-	}
-	sqlDB.SetMaxIdleConns(10)
+	database.Connect()
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
